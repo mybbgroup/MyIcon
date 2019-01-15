6928e723a57a57ce1fe65590c943d1241afe97ec
effone <effone@mybb.com>
2019-01-15 12:10:57 +0530

    fontforge implementation

--- gruntfile.js
+++ gruntfile.js
@@ -1,18 +1,18 @@
-module.exports = function(grunt) {
+module.exports = function (grunt) {
 	// Configuration.
 	grunt.initConfig({
 		grunticon: {
 			myIcon: {
-					files: [{
-						expand: true,
-						cwd: 'src/svgs',
-						src: ['*.svg', '*.png'],
-						dest: "dist"
-					}],
+				files: [{
+					expand: true,
+					cwd: 'src/svgs',
+					src: ['*.svg', '*.png'],
+					dest: "dist"
+				}],
 				options: {
 					loadersnippet: "grunticon.loader.js",
 					defaultWidth: "24px",
-					defaultHeight: "24px",					
+					defaultHeight: "24px",
 					previewTemplate: "src/preview.hbs",
 					cssprefix: ".icon-"
 				}
@@ -23,12 +23,17 @@ module.exports = function(grunt) {
 				src: 'src/svgs/*.svg',
 				dest: 'dist/fonts',
 				options: {
-					engine: "node"
+					//engine: "node", // Don't use node, it always fails to render svgo optimized vectors
+					font: "MyIcon",
+					syntax: "bootstrap",
+					templateOptions: {
+						classPrefix: 'icon-'
+					}
 				}
 			}
 		}
 	});
 	grunt.loadNpmTasks('grunt-grunticon');
 	grunt.loadNpmTasks('grunt-webfont');
-	grunt.registerTask('default', ['grunticon:myIcon','webfont:myIcon']);
+	grunt.registerTask('default', ['grunticon:myIcon', 'webfont:myIcon']);
 };
